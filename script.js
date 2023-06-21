9f731066f92d7e797d0f9903dc7a1fb38de6ca6f
Prince <princeraj.chauhan@outlook.com>
2023-06-22 04:23:54 +0530

    Day3 Complete

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let head = document.getElementsByClassName('head')[0]
 
 let firstDay = document.getElementById('firstDay')
 let secondDay = document.getElementById('secondDay')
+let thirdDay = document.getElementById('thirdDay')
 
 
 function day1() {
@@ -11,11 +12,21 @@ function day1() {
     head.style.color = "aqua"
     firstDay.style.display = "block"
     secondDay.style.display = "none"
+    thirdDay.style.display = "none"
 }
 
 function day2() {
     head.innerText = "Day - 2"
     head.style.color = "yellow"
-    firstDay.style.display = "none"
     secondDay.style.display = "block"
+    firstDay.style.display = "none"
+    thirdDay.style.display = "none"
+}
+
+function day3() {
+    head.innerText = "Day - 3"
+    head.style.color = "orchid"
+    secondDay.style.display = "none"
+    firstDay.style.display = "none"
+    thirdDay.style.display = "block"
 }
